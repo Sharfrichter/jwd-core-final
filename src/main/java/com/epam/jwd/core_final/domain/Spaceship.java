0d2dc139dd3739f779e326c37fef9715528d6e06
Sharfrichter <pozhivilkopetka@gmail.com>
2021-02-26 18:20:26 +0300

    added fields to Spaceship class

--- src/main/java/com/epam/jwd/core_final/domain/Spaceship.java
+++ src/main/java/com/epam/jwd/core_final/domain/Spaceship.java
@@ -1,10 +1,43 @@
 package com.epam.jwd.core_final.domain;
 
+import java.util.Map;
+
 /**
  * crew {@link java.util.Map<Role, Short>}
  * flightDistance {@link Long} - total available flight distance
  * isReadyForNextMissions {@link Boolean} - true by default. Set to false, after first failed mission
  */
 public class Spaceship extends AbstractBaseEntity {
+    Map<Role,Short> crew;
+    Long flightDistance;
+    Boolean isReadyForNextMissions;
+
+    public Spaceship() {
+        isReadyForNextMissions = true;
+    }
+
+    public Map<Role, Short> getCrew() {
+        return crew;
+    }
+
+    public void setCrew(Map<Role, Short> crew) {
+        this.crew = crew;
+    }
+
+    public Long getFlightDistance() {
+        return flightDistance;
+    }
+
+    public void setFlightDistance(Long flightDistance) {
+        this.flightDistance = flightDistance;
+    }
+
+    public Boolean getReadyForNextMissions() {
+        return isReadyForNextMissions;
+    }
+
+    public void setReadyForNextMissions(Boolean readyForNextMissions) {
+        isReadyForNextMissions = readyForNextMissions;
+    }
     //todo
 }
